2c27af0d84c8b35ed8fd98c31533a920d4c4f918
Adarsh Das <165044669+AdarshDas07@users.noreply.github.com>
2025-11-07 23:52:09 -0600

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,8 +20,8 @@ const Layout: React.FC = () => {
       <header className="bg-white border-b border-gray-200 sticky top-0 z-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
-                <div className="text-2xl font-bold tracking-tight text-gray-900">
-                <NavLink to="/">Adarsh Das</NavLink>
+                <div className="text-xl font-bold tracking-tight text-gray-900">
+                <NavLink to="/">Adarsh Das's Academic Portfolio</NavLink>
                 </div>
                 <nav className="hidden md:flex items-center space-x-8">
                 {navLinks.map((link) => (
